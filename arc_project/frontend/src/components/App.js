fc18478927bc7e8d91a553698fba923ae1e1ec80
Hiep Phan <phan.hiepq@gmail.com>
2020-02-22 09:17:06 -0500

    revert to original

--- arc_project/frontend/src/components/App.js
+++ arc_project/frontend/src/components/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-import Login from "./Login/login";
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -35,16 +33,15 @@ class App extends Component {
 
   render() {
     return (
-      // <ul>
-      //   {this.state.data.map(contact => {
-      //     return (
-      //       <li key={contact.id}>
-      //         {contact.first_name} - {contact.last_name} - {contact.email}
-      //       </li>
-      //     );
-      //   })}
-      // </ul>
-      <Login/>
+      <ul>
+        {this.state.data.map(contact => {
+          return (
+            <li key={contact.id}>
+              {contact.first_name} - {contact.last_name} - {contact.email}
+            </li>
+          );
+        })}
+      </ul>
     );
   }
 }
