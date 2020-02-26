879b3b7159a237968f429ac110e8472b2a156870
megjaffy <jaffy_m1@denison.edu>
2020-02-26 15:44:51 -0500

    Adding folder, javascript and .css file for contract form

---
+++ arc_project/frontend/src/components/Main/TutorContracts/TutorContractForm/tutor-contract-form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import css from './tutor-contract-item.module.css';
+
+export default class TutorContractItem extends Component {
+
+  render() {
+    return (
+      </>
+    );
+  }
+}
