be09e9cb140eda3becb5ca57ab3347fd4d2837f8
megjaffy <jaffy_m1@denison.edu>
2020-04-13 12:48:41 -0400

    repopulate database

--- arc_project/frontend/src/components/Auth/auth.js
+++ arc_project/frontend/src/components/Auth/auth.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const config = {
   googleClientId: '903588499024-e233icds29no0o6h89n01tgvdv7u5dv4.apps.googleusercontent.com',
-  dbClientId: 'IzW5nvTbN0KLY9rTHJ0ZTb11tInqU7KVt5pWedNc',
+  dbClientId: 'JKKlBN2xGJHklx5hh6NDzxaEbaWGc0bQh3Ute2ts',
   scope: 'email',
 };
 
