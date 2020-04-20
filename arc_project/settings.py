bc8d03015717b7d87075de7fb906be7dbe636651
quang2705 <minhquang2705@gmail.com>
2020-04-20 13:46:53 -0400

    fixbug

--- arc_project/settings.py
+++ arc_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'frontend',
     'oauth2_provider',
     'social_django',
-    'rest_framework_social_oauth2',s
+    'rest_framework_social_oauth2',
     'whitenoise.runserver_nostatic'
 ]
 
