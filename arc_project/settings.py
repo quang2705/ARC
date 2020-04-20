b2209dc27108922558054366ab38676b07ae6585
quang2705 <minhquang2705@gmail.com>
2020-04-20 13:16:23 -0400

    whitenoise and degrade django

--- arc_project/settings.py
+++ arc_project/settings.py
@@ -126,6 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
+
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static'),
+]
 
 #REST API configuration
 REST_FRAMEWORK = {
