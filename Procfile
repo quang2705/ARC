7b47203ff279c9b0145853c9f8e53538e14f9cb6
quang2705 <minhquang2705@gmail.com>
2020-04-20 12:19:30 -0400

    heroku config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djangoheroku.wsgi
--- arc_project/settings.py
+++ arc_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -158,3 +158,5 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
 ]
 #Login redirect url
 LOGIN_REDIRECT_URL = '/api'
+
+django_heroku.settings(locals())
