e55687c0d7f36ea0796c430e8803e44bcd378ebd
Hiep Phan <phan.hiepq@gmail.com>
2020-04-21 20:25:05 -0400

    mod(login): redesign UI

--- frontend/src/components/Login/login.js
+++ frontend/src/components/Login/login.js
@@ -21,9 +21,21 @@ export default class Login extends Component {
     return (
       <div className={css.container}>
         <div className={css.panel}>
-          <div className={css.title}>Login to Denison ARC tutoring</div>
-          <div className={css.subtitle}>Please use your Denison email</div>
-          <div id='my-signin2' className={css.signinButton}/>
+          <div>
+            <div className={css.title}>Login</div>
+            <div className={css.subtitle}>Denison ARC Tutoring</div>
+            <div id='my-signin2' className={css.signinButton}/>
+            <div className={css.helpText}>Please use your Denison email</div>
+            <div className={css.warning}>
+              By signing in, you agree to give us permission to use your email address to send emails to your tutees, headtutors, and ARC administrators.
+              We will only send emails regarding your tutoring contracts, sessions and session verification and will not disclose your personal information.
+            </div>
+          </div>
+
+          <div className={css.bottom}>
+            <a href='https://my.denison.edu' rel="noopener noreferrer" target="_blank">MyDenison</a>
+            <a href='https://wiki.denison.edu/Tutoring/TutorRequest' rel="noopener noreferrer" target="_blank">Request a tutor</a>
+          </div>
         </div>
 
       </div>
