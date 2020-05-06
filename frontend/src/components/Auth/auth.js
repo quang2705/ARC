eeab1ad6a565c4affd7d76503cf0e10ac77fd969
quang2705 <minhquang2705@gmail.com>
2020-05-06 10:22:51 -0400

    double check

--- frontend/src/components/Auth/auth.js
+++ frontend/src/components/Auth/auth.js
@@ -3,6 +3,7 @@ import React from 'react';
 const config = {
   googleClientId: '903588499024-e233icds29no0o6h89n01tgvdv7u5dv4.apps.googleusercontent.com',
   dbClientId: 'xW0xfkdwK0Qyp6JClcjWWb1Yb709THgaiXXm9E97',
+  // dbClientId: 'YDmh3HtMW4drVvgWPgdlRP5KkaWl9nCag1I406IJ',
   scope: 'https://www.googleapis.com/auth/gmail.send',
 };
 
--- post_compile.sh
+++
@@ -1,2 +0,0 @@
-python manage.py makemigrations arc_app
-python manage.py migrate
