e148fddd3df393db0aab07357cfbbd112f6220e2
quang2705 <minhquang2705@gmail.com>
2020-05-07 11:27:18 -0400

    fixing auth

--- frontend/src/components/Auth/auth.js
+++ frontend/src/components/Auth/auth.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const config = {
   googleClientId: '903588499024-e233icds29no0o6h89n01tgvdv7u5dv4.apps.googleusercontent.com',
-  // dbClientId: 'xW0xfkdwK0Qyp6JClcjWWb1Yb709THgaiXXm9E97',
-  dbClientId: 'xBWkmuIenPIPoRhogyIjZD5Y8kTAbXUQecJ1FCBG',
+  dbClientId: 'xW0xfkdwK0Qyp6JClcjWWb1Yb709THgaiXXm9E97',
   scope: 'https://www.googleapis.com/auth/gmail.send',
 };
 
