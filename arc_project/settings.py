87729cf5e3ee4095b12f0eaabf9d2014f641d99b
quang2705 <minhquang2705@gmail.com>
2020-05-08 11:59:22 -0400

    switch back to debug = False

--- arc_project/settings.py
+++ arc_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '93*@wbdf51ts+wv!avf#rzo&rq4j@($-shz%7uhgporc@runwz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
